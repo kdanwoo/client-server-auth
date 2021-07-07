22db972124023c0b86384aa1a9def98e36f9b8a3
danwook <k.danwoo91@ibksystem.co.kr>
2021-07-07 15:20:38 +0900

    Initial commit

---
+++ src/main/java/com/example/clientserverauth/ClientServerAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.clientserverauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientServerAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientServerAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clientserverauth/ClientServerAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clientserverauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServerAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
